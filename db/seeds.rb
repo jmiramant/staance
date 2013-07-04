e2e32ddc2367a83a0b40257c055979ec5046ff40
Apprentice <apprentice@dbc21.local>
2013-07-04 11:25:22 -0700

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
+# ruby encoding: utf-8
+
 TOPICS = ["Community", "Diversity", "Economy", "Environment",
 "Guns & Crime", "Health", "Immigration", "International",
 "LGBTQQ", "Military", "Parenting", "Politics", "Science & Technology",
 "Spanish", "Women", "Employment"]
 
-TOPICS.each { |topic| Topic.create(title: topic) }
+TOPICS.each { |topic| Topic.find_or_create_by_title(topic) }
 
+lloyd = User.create(email: "ltaylor@netelder.com", password: "password")
+josh = User.create(email: "josh@miramant.me", password: "password")
+
+campaign1 = Campaign.new(title: "Campaign Number 1", short_blurb: "Blurb for Campaign Number 1")
+campaign1.location = "San Francisco"
+campaign1.cause_url = "http://www.yahoo.com"
+campaign1.funding_deadline = Time.now
+campaign1.topic = Topic.find(3)
+campaign1.save
+CampaignUser.create(campaign_id: campaign1.id, user_id: lloyd.id, user_type: "Creator")
