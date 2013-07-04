6e18e4db47900767e740a0f6a56a31de22e8cfdb
Apprentice <apprentice@dbc21.local>
2013-07-04 15:28:25 -0700

    add list of supported campaigns to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    supports = CampaignUser.where(user_type: "Supporter", user_id: @user.id).pluck("campaign_id")
+    @campaigns = []
+    if supports
+      supports.each do |campaign_id|
+        @campaigns << Campaign.find(campaign_id)
+      end
+    end
   end
 end
