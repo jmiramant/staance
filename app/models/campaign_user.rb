b55cc6a5b5832bd9c3dd49d3a6baf859bc16b615
dbc-apprentice <dbc@dbc.com>
2013-07-04 13:20:57 -0700

    campaign validations

--- app/models/campaign_user.rb
+++ app/models/campaign_user.rb
@@ -2,7 +2,7 @@ class CampaignUser < ActiveRecord::Base
   attr_accessible :user_type, :campaign_id, :user_id
   
   validates_presence_of :campaign_id, :user_id
-  validates :user_type, uniqueness: {scope: [:campaign_id, :user_id]}
+  validates_uniqueness_of :user_type, scope: [:campaign_id, :user_id]
 
   belongs_to	:user
   belongs_to	:campaign
