59f6cb44455e169b6cea2b3f03194d16034d94c8
Apprentice <apprentice@devbootcamp.com>
2013-07-06 16:27:00 -0700

    WIP

--- app/assets/javascripts/donation.js
+++ app/assets/javascripts/donation.js
@@ -1,7 +1,13 @@
+function makePayment() {
+  setTimeout(function() {  $('.stripe-button-el').click()}, 100); 
+  setTimeout(function() {  $('.stripe_payment').remove() }, 200);
+}
+
 $(document).ready(function(){
+
   $('.donate_payment').on('ajax:success', function(e, data){
-    $('.donate').append(data);
-    setTimeout(function() {  $('.stripe-button-el').click() }, 100);
-    setTimeout(function() {  $('.stripe_payment').remove() }, 200);
+    $('.donate').append(data)
+    makePayment();
   });
+
 });
