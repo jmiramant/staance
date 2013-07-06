de785a4e65fed86f98e77125f33bac3ed0984a32
Apprentice <apprentice@dbc21.local>
2013-07-06 13:54:32 -0700

    added admin true for josh user

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ TOPICS.each { |topic| Topic.find_or_create_by_title(topic) }
 
 lloyd = User.create(email: "ltaylor@netelder.com", password: "password", name: "Lloyd Taylor")
 josh = User.create(email: "josh@miramant.me", password: "password", name: "Josh Miramant")
+josh.admin = true
+josh.save
 
 campaign1 = Campaign.new(title: "Campaign Number 1", short_blurb: "Blurb for Campaign Number 1")
 campaign1.location = "San Francisco"
