92310610965f485c3dbcc2e58c175f1e304e952e
Apprentice <apprentice@dbc24.local>
2013-07-06 18:32:24 -0700

    carousel by click works

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,29 @@
+var Carousel = {
+  current: 0,
+
+  init: function() {
+    $('.arrow.left').on('click', function() { Carousel.changeVideo(-1); });
+    $('.arrow.right').on('click', function() { Carousel.changeVideo(1); });
+  },
+
+  changeVideo: function(num) {
+    $('.featured-video.' + this.current).hide();
+    $('.icon-circle.' + this.current).css('color', '#545454');
+    this.current = (this.current + num + 3) % 3;
+    $('.featured-video.' + this.current).fadeIn();
+    $('.icon-circle.' + this.current).css('color', '#fece12');
+  }
+};
+
 $(document).ready(function(){
+  // featured video click & carousel
+  Carousel.init();
+
+
+
+
+
+  // campaign grid
   $('.select-bar a').on('click', function(e) {
     e.preventDefault();
     var klass = $(this).attr('id');
@@ -6,5 +31,9 @@ $(document).ready(function(){
     $(this).addClass("selected");
     $('.campaign-snapshot').hide();
     $('.' + klass).fadeIn('slow');
-  })
+  });
+
+
+
+
 });
