9d6ac9e1b0d9e851896b41be888d678845f585dd
Apprentice <apprentice@dbc24.local>
2013-07-08 16:38:40 -0700

    cleaned up social.js

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -1,19 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	$('.fb-like').attr("data-href", window.location.href);
   $('.fb-send').attr("data-href", window.location.href);
 });
+
 window.fbAsyncInit = function() {
   FB.init({
     appId      : 622874237732350,
     channelUrl : '//WWW.LOCALHOST:3000/',
     status     : true,
     xfbml      : true
-    });
-  };
-(function(d, s, id){
+  });
+};
+
+(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=622874237732350";
   fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));
