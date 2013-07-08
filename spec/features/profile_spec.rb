d2b389f2d0e3a79e6240b98e67f3db7561a5a28c
Lloyd Taylor <ltaylor@netelder.com>
2013-07-07 21:20:13 -0700

    add rspec for profile edit

---
+++ spec/features/profile_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+
+describe "Profile" do
+
+  include FeatureHelpers
+
+  before do
+    @user = create(:user)
+    login(@user)
+  end
+
+  it "can update user profile", js: true do
+    visit edit_user_registration_path
+    fill_in "user_about_me", with: "I like kittens"
+    fill_in "user_current_password", with: "password"
+    click_on "Update"
+    expect(page).to have_content "User Information"
+  end
+end
