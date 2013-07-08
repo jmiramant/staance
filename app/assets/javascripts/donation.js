fa44267324b3fdd8ea1717c2905211c0109fd4c3
Apprentice <apprentice@dbc24.local>
2013-07-08 16:22:53 -0700

    cleaned up donation.js

--- app/assets/javascripts/donation.js
+++ app/assets/javascripts/donation.js
@@ -1,16 +1,15 @@
 function makePayment() {
-  setTimeout(function() {  $('.stripe-button-el').click()}, 100); 
-  setTimeout(function() {  $('.stripe_payment').remove() }, 200);
+  setTimeout(function() { $('.stripe-button-el').click(); }, 100);
+  setTimeout(function() { $('.stripe_payment').remove(); }, 200);
 }
 
-$(document).ready(function(){
-
-  $('.donate_payment').on('ajax:success', function(e, data){
+$(document).ready(function() {
+  $('.donate_payment').on('ajax:success', function(e, data) {
     $('.donate').append(data);
     makePayment();
   });
 
-  $('.donate_payment').on('ajax:error', function(e, data){
-    alert("Please log in to make a donation");    
+  $('.donate_payment').on('ajax:error', function() {
+    alert("Please log in to make a donation");
   });
 });
