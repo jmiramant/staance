3d479883b062094dc5b274c15369eacb7a416009
Apprentice <apprentice@dbc21.local>
2013-07-08 19:51:10 -0700

    change :short_blurb to t.text

--- db/migrate/20130703204824_create_campaigns.rb
+++ db/migrate/20130703204824_create_campaigns.rb
@@ -2,10 +2,10 @@ class CreateCampaigns < ActiveRecord::Migration
   def change
     create_table :campaigns do |t|
     	t.references	:topic
-      t.string      :title, :image_url, :short_blurb, :location, :video_url, :cause_url
+      t.string      :title, :image_url, :location, :video_url, :cause_url
     	t.datetime	  :funding_deadline
     	t.integer		  :funding_goal, default: 1
-    	t.text			  :pitch  
+    	t.text			  :pitch, :short_blurb
     end
   end
 end
