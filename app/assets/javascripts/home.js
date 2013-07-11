06f16f89672ba8ab7f79ce39c49dd09bfe3d80f9
Apprentice <apprentice@dbc21.local>
2013-07-10 18:28:39 -0700

    dry-ed up home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,31 +5,20 @@ var Carousel = {
     $('#arrow.left').on('click', function() { Carousel.changeVideo(-1); });
     $('#arrow.right').on('click', function() { Carousel.changeVideo(1); });
     $('.icon-circle').on('click', function(e) {
-        Carousel.changeVideoAbs(e.currentTarget.classList[1]);
+        Carousel.changeVideo(e.currentTarget.classList[1], true);
     });
   },
 
-  // DRY this out somehow
-
-  changeVideo: function(num) {
+  changeVideo: function(num, abs) {
     $('#featured-video-image.' + this.current).hide();
     $('#featured-video-details.' + this.current).hide();
     $('.icon-circle.' + this.current).removeClass('selected');
-    this.current = (this.current + num + 3) % 3;
+    if (abs == null) { this.current = (this.current + num + 3) % 3; }
+    else { this.current = num; }
     $('#featured-video-image.' + this.current).fadeIn(1000);
     $('#featured-video-details.' + this.current).fadeIn(1000);
     $('.icon-circle.' + this.current).addClass('selected');
   },
-
-  changeVideoAbs: function(num) {
-    $('#featured-video-image.' + this.current).hide();
-    $('#featured-video-details.' + this.current).hide();
-    $('.icon-circle.' + this.current).removeClass('selected');
-    this.current = num;
-    $('#featured-video-image.' + this.current).fadeIn(1000);
-    $('#featured-video-details.' + this.current).fadeIn(1000);
-    $('.icon-circle.' + this.current).addClass('selected');
-  }
 };
 
 $(document).ready(function() {
