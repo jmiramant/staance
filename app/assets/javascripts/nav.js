d7d7d22e3bc3fa3e41a00d43f5f31c581b08af7b
Apprentice <apprentice@dbc19.local>
2013-07-10 17:16:27 -0700

    updated nav

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,3 +1,33 @@
-$(document).ready(function() {  
-  
+$(document).ready(function() { 
+  $(".logo").mouseover(function(){
+    $("#take").animate({ opacity: 1}, 50);
+    $("#stance").animate({ opacity: 1}, 50);
+    $("#an_a").css('color', '#acacac');
+  });
+  $(".logo").mouseleave(function(){
+    $("#take").animate({ opacity: 0}, 50);
+    $("#stance").animate({ opacity: 0}, 50);
+    $("#an_a").css('color', '#FF3765');
+
+  });
+  $(window).scroll(function(){ 
+  if ($(this).scrollTop() > 10){  
+    $('nav').addClass("short_nav");
+    $('.nav-links').addClass("links_up");
+    $('#take').addClass("take_down");
+    $('#stance').addClass("stance_up");
+    $('#camp').addClass("be_gone");
+    $('.create-campaign').addClass('smr_button');
+  } 
+  else{
+    $('nav').removeClass("short_nav");
+    $('.nav-links').removeClass("links_up");
+    $('#take').removeClass("take_down");
+    $('#stance').removeClass("stance_up");
+    $('.create-campaign').removeClass('smr_button');
+    $('#camp').removeClass("be_gone");
+  }
+  });
+
 });  
+
